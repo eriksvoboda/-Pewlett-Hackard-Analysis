9d58cb8f9d22ac7e08e54622faba21a3db1dbe6c
eriksvoboda <93800235+eriksvoboda@users.noreply.github.com>
2022-02-06 15:44:00 -0600

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -36,12 +36,13 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -49,6 +50,56 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (title)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date)
 );
+
+-- RETIREMENT ELIGIBILITY
+-- Notice how the second condition is inside parentheses? This is a tuple; in Python, data can be stored inside a tuple and accessed in the 
+-- same way as a list. In SQL, the tuples in this block of code are part of the syntax. They basically place each condition in a group
+-- and Postgres looks for the first group firs, then looks inside the second group to deleiver the second condition.
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- COUNT THE NUMBER OF EMPLOYEES RETIRING
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- In PSQL only data that is saved as a table can be easily exported, modifiy the code below to make a new table so it can be exported
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-->
+-- SELECT INTO tells psql that instead of generating a list of results the data is saved as a new table completely
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- CREATE NEW TABLE FOR RETIRING EMPLOYEES
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- CHECK THE TABLE
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+-- seleect statement selects only the columns we want to view from each table
+-- From statement points to the first table to be joined, Departments (Table 1)
+-- INNER JOIN points to the second table to be joined, dept_manager (Table 2)
+-- ON deparemtns.dept_no = managers.dept_no; indicates where PSQL should look for matches
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date,
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
